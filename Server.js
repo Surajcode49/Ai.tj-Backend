beea4c52748be63790b322910f0f6a9e1f1624c1
Suraj kumar <devilsuraj9641@gmail.com>
2024-03-10 21:27:53 +0530

    sjj

--- Server.js
+++ Server.js
@@ -1,9 +1,13 @@
 import app from "./app.js";
+import { connectDB } from "./config/database.js";
+
+connectDB();
 
 
-app.get("/",(req,res)=>{
-  res.send("<h1>hii this is suraj prasad</h1>")
-})
+
+app.get("/", (req, res,next) => {
+  res.send("<h1>hii this is suraj prasad</h1>");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`server is Running bro at PORT: ${process.env.PORT}`);
--- app.js
+++ app.js
@@ -7,3 +7,8 @@ export default app;
 dotenv.config({
   path: "./config/config.env",
 });
+
+// importing Routes
+import userRoute from "./routes/user.js";
+
+app.use("/api/v1", userRoute);
