e5213c6efca126de8d537462edda8bddc860c5b9
Suraj kumar <devilsuraj9641@gmail.com>
2024-03-10 22:50:19 +0530

    noo

--- utils/provider.js
+++ utils/provider.js
@@ -3,10 +3,23 @@ import passport from "passport";
 
 export const connectPassport = () => {
   passport.use(
-    new GoogleStrategy({
-      clientID: "asd",
-      ClientSecret: "asd",
-      callbackURL: "asd",
-    })
+    new GoogleStrategy(
+      {
+        clientID: "asd",
+        ClientSecret: "asd",
+        callbackURL: "asd",
+      },
+      async function (accessToken, refreshToken, profile, done) {
+        // Data comes here
+      }
+    )
   );
 };
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  done(null, user);
+});
